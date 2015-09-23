29be00000fda8dfe894cdfcc26697ac9c930ae74
Moligaloo <moligaloo@gmail.com>
2015-09-23 13:44:06 +0800

    lots of update

--- citrus.lua
+++ citrus.lua
@@ -1,29 +1,5 @@
 local re = require 're'
 
-local grammar = re.compile [[
-	statements <- {| statement+ |}
-	
-	statement <- comment / empty / create_table / insert
-	empty <- { [%nl] } 
-	comment <- { '--' [^%nl]+ %nl }
-	
-	create_table <- {| {:type: '' -> 'create_table':} '+@' s {:table_name:identifier:} {:optional:'?'?:} {:columns:column_defs:} %nl |}
-	identifier <- [_a-zA-Z][_a-zA-Z0-9]+
-	column_defs <- '(' s {| column_def+ |} s ')'
-	column_def <- {| {:column_name:identifier:} s ':' s {:column_type:column_type:} |} / (',' s column_def)
-	column_type <- {| {:base_name:identifier:} {:postfixes:column_type_postfixes:} |}
-	column_type_postfix <- '!' / '?' / '++'
-	column_type_postfixes <- {| {column_type_postfix}* |} 
-	s <- [ ]*
-	
-	insert <- {| {:type: '' -> 'insert':} '+' {:table_name:identifier:} {:key_values:key_values:} |}
-	key_values <- '(' s {| key_value+ |} s ')'
-	key_value <- {| {:key:identifier:} s ':' s {:value:value:} |} / (',' s key_value)
-	value <- integer_literal / string_literal
-	integer_literal <- [0-9]+
-	string_literal <- '"' [^"]+ '"'
-]]
-
 local function collect_values(func, sep, ...)
 	local co = coroutine.create(func)
 	local words = {}
@@ -90,55 +66,80 @@ local function column_defs_from_columns(columns)
 	)
 end
 
-local function to_sqlite(content)
-	local statements = grammar:match(content)
-	if statements == nil then
-		return
+local grammar_string = [[
+	statements <- {| statement+ |}
+	
+	statement <- comment / empty / create_table / insert
+	empty <- { [%nl] } 
+	comment <- { '--' [^%nl]+ %nl }
+	
+	create_table <- ({< '+@' {:optional:'?'?:} s {:table_name:identifier:}  {:columns:column_defs:}  >} %nl) -> create_table
+	identifier <- [_a-zA-Z][_a-zA-Z0-9]+
+	column_defs <- '(' s {| column_def+ |} s ')'
+	column_def <- {| {:column_name:identifier:} s ':' s {:column_type:column_type:} |} / (',' s column_def)
+	column_type <- {| {:base_name:identifier:} {:postfixes:column_type_postfixes:} |}
+	column_type_postfix <- '!' / '?' / '++'
+	column_type_postfixes <- {| {column_type_postfix}* |} 
+	s <- [%s]*
+	
+	insert <- {< '+' {:table_name:identifier:} {:key_values:key_values:} >} -> insert
+	key_values <- '(' s {| key_value+ |} s ')'
+	key_value <- {| {:key:identifier:} s ':' s {:value:value:} |} / (',' s key_value)
+	value <- integer_literal / string_literal
+	integer_literal <- [0-9]+
+	string_literal <- '"' [^"]+ '"'
+]]
+
+grammar_string = grammar_string:gsub('{<', '{| {:start:{}:} '):gsub('>}', '{:finish:{}:} |}')
+
+local grammar = re.compile(grammar_string, {
+	create_table = function(statement)
+		statement.value = 
+			("create table %s%s(%s);\n"):format(
+				statement.table_name,
+				statement.optional == '?' and ' if not exists' or '',
+				column_defs_from_columns(statement.columns)
+			)
+
+		return statement
+	end,
+	insert = function(statement)
+		local keys, values = {}, {}
+		for _, pair in ipairs(statement.key_values) do
+			table.insert(keys, pair.key)
+			table.insert(values, pair.value)
+		end
+
+		statement.value = (("insert into %s(%s) values(%s);"):format(
+			statement.table_name,
+			table.concat(keys, ', '),
+			table.concat(values, ', ')
+		))
+
+		return statement
 	end
+})
 
-	return collect_values(
-		function()
-			for _, statement in ipairs(statements) do
-				if type(statement) == 'string' then
-					yield(statement)
+local function to_sqlite(content, options)
+	options = options or { keep_citrus_as_comment = true}
+
+	local statements = grammar:match(content)
+	if statements then
+		local strings = {}
+		for _, statement in ipairs(statements) do
+			if type(statement) == 'string' then
+				table.insert(strings, statement)
+			else
+				if options.keep_citrus_as_comment then
+					local start, finish = statement.start, statement.finish
+					table.insert(strings, '-- ' .. content:sub(start, finish))
 				end
-
-				local type = statement.type
-				if type == 'comment' then
-					yield("--" .. statement.content)
-				elseif type == 'empty' then
-					yield ''
-				elseif type == 'create_table' then
-					yield(
-						collect_values(
-							function() 
-								yield 'create table '
-								yield(statement.table_name)
-								if statement.optional == '?' then
-									yield ' if not exists'
-								end
-								yield(("(%s);\n"):format(column_defs_from_columns(statement.columns)))
-							end, 
-							''
-						)
-					)
-				elseif type == 'insert' then
-					local keys, values = {}, {}
-					for _, pair in ipairs(statement.key_values) do
-						table.insert(keys, pair.key)
-						table.insert(values, pair.value)
-					end
-
-					yield(("insert into %s(%s) values(%s);"):format(
-						statement.table_name,
-						table.concat(keys, ', '),
-						table.concat(values, ', ')
-					))
-				end
+				table.insert(strings, statement.value)
 			end
-		end,
-		''
-	)
+		end
+
+		return table.concat(strings)
+	end
 end
 
 return {
