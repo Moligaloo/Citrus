9b9335bb97f9921b002a8a7cadfe274c89dfe137
Moligaloo <moligaloo@gmail.com>
2015-09-25 22:29:18 +0800

    use ae function to make test code shorter

--- test.lua
+++ test.lua
@@ -1,53 +1,51 @@
 #!/usr/bin/env lua
 
-EXPORT_ASSERT_TO_GLOBALS = true
 local luaunit = require 'luaunit'
+local assertEquals = luaunit.assertEquals
 
 local citrus = require 'citrus'
-local convert = function(citrus_code)
-	return citrus.to_sqlite(citrus_code, {keep_citrus_as_comment = false})
+local ae = function(citrus_code_without_newline, sql_code_without_semicolon_and_newline)
+	local citrus_code = citrus_code_without_newline .. '\n'
+	local result_code = citrus.to_sqlite(citrus_code, {keep_citrus_as_comment = false})
+	assertEquals(result_code, sql_code_without_semicolon_and_newline .. ';\n')
 end
 
 function test_create_table()
-	assertEquals(
-		convert(
-			'+@?users(id: integer!++, name:text, gender:integer, location: text?)\n'
-		), 
-		'create table if not exists users(\n\tid integer primary key autoincrement,\n\tname text not null,\n\tgender integer not null,\n\tlocation text\n);\n'
+	ae(
+		'+@?users(id: integer!++, name:text, gender:integer, location: text?)',
+		'create table if not exists users(\n\tid integer primary key autoincrement,\n\tname text not null,\n\tgender integer not null,\n\tlocation text\n)'
 	)
 
-	assertEquals(
-		convert(
-			'+@users(id: integer!++, name:text, gender:integer, location: text?)\n'
-		), 
-		'create table users(\n\tid integer primary key autoincrement,\n\tname text not null,\n\tgender integer not null,\n\tlocation text\n);\n'
+	ae(
+		'+@users(id: integer!++, name:text, gender:integer, location: text?)',
+		'create table users(\n\tid integer primary key autoincrement,\n\tname text not null,\n\tgender integer not null,\n\tlocation text\n)'
 	)
 end
 
 function test_drop_table()
-	assertEquals(convert('-@users\n'), 'drop table users;\n')
-	assertEquals(convert('-@?users\n'), 'drop table if exists users;\n')
+	ae('-@users', 'drop table users')
+	ae('-@?users', 'drop table if exists users')
 end
 
 function test_insert()
-	assertEquals(convert('+users(name: "Moligaloo", gender: "male")\n'), 'insert into users(name, gender) values("Moligaloo", "male");\n')
+	ae('+users(name: "Moligaloo", gender: "male")', 'insert into users(name, gender) values("Moligaloo", "male")')
 end
 
 function test_select()
-	assertEquals(convert('users\n'), 'select * from users;\n')
-	assertEquals(convert('*@users\n'), 'select * from users;\n')
-	assertEquals(convert('name@users#123\n'), 'select name from users where id = 123;\n')
-	assertEquals(convert('name@users\n'), 'select name from users;\n')
-	assertEquals(convert('name@users>id\n'), 'select name from users order by id desc;\n')
-	assertEquals(convert('name@users>id<name\n'), 'select name from users order by id desc, name asc;\n')
-	assertEquals(convert('name@users^10\n'), 'select name from users limit 10;\n')
-	assertEquals(convert('name@users+20\n'), 'select name from users offset 20;\n')
-	assertEquals(convert('name@users+20^10\n'), 'select name from users limit 10 offset 20;\n')
-	assertEquals(convert('id,name@users\n'), 'select id, name from users;\n')
+	ae('users', 'select * from users')
+	ae('*@users', 'select * from users')
+	ae('name@users#123', 'select name from users where id = 123')
+	ae('name@users', 'select name from users')
+	ae('name@users>id', 'select name from users order by id desc')
+	ae('name@users>id<name', 'select name from users order by id desc, name asc')
+	ae('name@users^10', 'select name from users limit 10')
+	ae('name@users+20', 'select name from users offset 20')
+	ae('name@users+20^10', 'select name from users limit 10 offset 20')
+	ae('id,name@users', 'select id, name from users')
 end
 
 function test_delete()
-	assertEquals(convert('-users[location = "Shanghai"]\n'), 'delete from users where location = "Shanghai";\n')
+	ae('-users[location = "Shanghai"]', 'delete from users where location = "Shanghai"')
 end
 
 os.exit(luaunit.LuaUnit.run())
